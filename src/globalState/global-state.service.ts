6a48b5cbf427250556eec5c4d0465850e3117424
DimitriySpotower <165659033+DimitriySpotower@users.noreply.github.com>
2025-02-09 19:35:57 +0200

    FEAT

--- src/globalState/global-state.service.ts
+++ src/globalState/global-state.service.ts
@@ -32,14 +32,20 @@ export class GlobalStateService implements OnModuleInit {
 
   async loadInitialData() {
     try {
-        let domains = await this.kidonClient('domain').select('*');
-        let companies = await this.kidonClient('companies').select('*');
-        const gptKey = await axios.get(`http://localhost:3000/secrets?secretName=kidonSecrets`,{headers: { Authorization: `Bearer ${process.env.KIDON_TOKEN}` }, } )
-        const allTokens = await axios.get(`http://localhost:3000/company/googleTokens`, {headers: { Authorization: `Bearer ${process.env.KIDON_TOKEN}` },})
+      const [domains, companies, gptKey, allTokens] = await Promise.all([
+        this.kidonClient('domain').select('*'),
+        this.kidonClient('companies').select('*'),
+        axios.get(`${process.env.KIDON_SERVER}/secrets?secretName=kidonSecrets`, {
+            headers: { Authorization: `Bearer ${process.env.KIDON_TOKEN}` },
+        }),
+        axios.get(`${process.env.KIDON_SERVER}/company/googleTokens`, {
+            headers: { Authorization: `Bearer ${process.env.KIDON_TOKEN}` },
+        }),
+    ]);
         this.setState('domains', domains);
         this.setState('companies', companies);
-        // this.setState('gptKey', gptKey.data.GPT_API_KEY);
-        // this.setState('allTokens', allTokens);
+         this.setState('gptKey', gptKey.data.GPT_API_KEY);
+         this.setState('allTokens', allTokens);
 
       console.log('✅ Global state initialized with data');
     } catch (error) {
