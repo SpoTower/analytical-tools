62633bd19201d44e9a6bc01f82611f20dc2c6f32
AitanSpotower <aitan@spotower.com>
2025-02-09 15:04:02 +0200

    feat: update global state service to include axios calls and change server port to 5000

--- src/globalState/global-state.service.ts
+++ src/globalState/global-state.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import axios from 'axios';
 import { Knex } from 'knex';
 import { ANALYTICS_CONNECTION, KIDON_CONNECTION } from 'src/knex/knex.module';
 
@@ -32,8 +33,8 @@ export class GlobalStateService implements OnModuleInit {
     try {
         let domains = await this.kidonClient('domain').select('*');
         let companies = await this.kidonClient('companies').select('*');
-        // const gptKey = await axios.get(`http://localhost:3000/secrets?secretName=kidonSecrets`,{headers: { Authorization: `Bearer ${process.env.KIDON_TOKEN}` }, } )
-        // const allTokens = await axios.get(`http://localhost:3000/company/googleTokens`, {headers: { Authorization: `Bearer ${process.env.KIDON_TOKEN}` },})
+        const gptKey = await axios.get(`http://localhost:3000/secrets?secretName=kidonSecrets`,{headers: { Authorization: `Bearer ${process.env.KIDON_TOKEN}` }, } )
+        const allTokens = await axios.get(`http://localhost:3000/company/googleTokens`, {headers: { Authorization: `Bearer ${process.env.KIDON_TOKEN}` },})
         this.setState('domains', domains);
         this.setState('companies', companies);
         // this.setState('gptKey', gptKey.data.GPT_API_KEY);
