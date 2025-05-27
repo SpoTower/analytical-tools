3f2cb6bff2c1bb33b2470114f4e77f97421030bb
DimitriySpotower <165659033+DimitriySpotower@users.noreply.github.com>
2025-05-27 18:11:09 +0300

    FEAT sweden

--- src/spell-checker/ignoreWords.ts
+++ src/spell-checker/ignoreWords.ts
@@ -4,7 +4,9 @@
 
 export const ignoredLanguages = [
     "spanish", 'esp', 'spain', 'italian',   'portugal', 'polish', 
-    'poland', 'brazil', "germany", 'german',   'france',   'Nutzungsbedingungen', 'easy-carrental', 'kartenratgeber', '/gr', '.gr', '/fr', '.fr'
-]
+    'poland', 'brazil', "germany", 'german',   'france',   'Nutzungsbedingungen', 'easy-carrental', 'kartenratgeber',
+     '/gr', '.gr', '/fr', '.fr', 'alternatives-casinostugan/', 'alternatives-hajper/', 'alternatives-snabbare/', 'sweden-sports-betting/',
+     'choosing-the-best-slots-games/', 'choosing-the-best-slots-games/', 'sweden'
+, ]
  
 export const ignoredDomains = ['funcasino.se']
