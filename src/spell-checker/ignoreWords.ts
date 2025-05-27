070a41ce411cb7a3218977f4a874320196bafcad
DimitriySpotower <165659033+DimitriySpotower@users.noreply.github.com>
2025-05-27 17:32:55 +0300

    FEAT removing gr fr and se

--- src/spell-checker/ignoreWords.ts
+++ src/spell-checker/ignoreWords.ts
@@ -3,6 +3,8 @@
 
 
 export const ignoredLanguages = [
-    "spanish", 'esp', 'spain', 'italian',   'portugal', 'polish', 'poland', 'brazil', "germany", 'german',   'france',   'Nutzungsbedingungen', 'easy-carrental', 'kartenratgeber'
+    "spanish", 'esp', 'spain', 'italian',   'portugal', 'polish', 
+    'poland', 'brazil', "germany", 'german',   'france',   'Nutzungsbedingungen', 'easy-carrental', 'kartenratgeber', '/gr', '.gr', '/fr', '.fr'
 ]
  
+export const ignoredDomains = ['funcasino.se']
