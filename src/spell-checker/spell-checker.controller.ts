0bfad51ebd989cdb93d6be5d737098a9b62025c3
DimitriySpotower <165659033+DimitriySpotower@users.noreply.github.com>
2025-06-03 17:10:30 +0300

    FEAT lineup validation should not searchion css

--- src/spell-checker/spell-checker.controller.ts
+++ src/spell-checker/spell-checker.controller.ts
@@ -62,7 +62,7 @@ export class SpellCheckerController {
 
    
  
- // fetches domain and uses axios to send requests to domain.paths
+ // fetches domain and uses pupeteer to send requests to domain.paths
 // checks whether there is a lineup on the page based on css class and id of lineup wrapper, and also that the status is 200 and the loading time less than 10 seconds
   @Get('/lineupValidation')
   async lineupValidation(
